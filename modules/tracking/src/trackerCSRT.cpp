f2144fafd3ef688e3508a66f05ce9b2689abab70
seovchinnikov <rubasketcom@gmail.com>
2019-01-09 18:50:41 +0300

    fixed displacement bug

--- modules/tracking/src/trackerCSRT.cpp
+++ modules/tracking/src/trackerCSRT.cpp
@@ -578,8 +578,8 @@ bool TrackerCSRTImpl::updateOnlyImpl(const Mat& image_)
 }
 
 void TrackerCSRTImpl::updateCenterAndScale(float dx, float dy, float dscale) {
-    object_center.x = object_center.x*dscale + dx*dscale;
-    object_center.y = object_center.y*dscale + dy*dscale;
+    object_center.x += dx;
+    object_center.y += dy;
 
     current_scale_factor *= dscale;
     dsst.current_scale_factor = current_scale_factor;
